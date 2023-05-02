06bbcab5a8057e85e069fa732385790ed4fbf316
Luke <ggeniussquid@gmail.com>
2023-05-02 09:50:04 +0800

    first_commit

---
+++ SQLtest.js
@@ -0,0 +1,261 @@
+const dataSQL = [
+    {
+        "type": "header",
+        "version": "5.1.2",
+        "comment": "Export to JSON plugin for PHPMyAdmin"
+    },
+    {
+        "type": "database",
+        "name": "mmmh57"
+    },
+    {
+        "type": "table",
+        "name": "address_book",
+        "database": "mmmh57",
+        "data": [
+            {
+                "sid": "1",
+                "name": "酷寶",
+                "email": "5781@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "2",
+                "name": "煞氣A健康的郭",
+                "email": "546@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "5",
+                "name": "李小明4",
+                "email": "5387@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "6",
+                "name": "李小明5",
+                "email": "5297@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "7",
+                "name": "李小明6",
+                "email": "324@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "8",
+                "name": "李小明7",
+                "email": "5729@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "9",
+                "name": "李小明8",
+                "email": "7673@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "10",
+                "name": "李小明9",
+                "email": "1179@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "13",
+                "name": "健氣☆ㄟ如",
+                "email": "2876@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "17",
+                "name": "李小明4",
+                "email": "841@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "18",
+                "name": "李小明5",
+                "email": "5580@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "19",
+                "name": "李小明6",
+                "email": "5375@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "20",
+                "name": "李小明7",
+                "email": "133@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "21",
+                "name": "李小明8",
+                "email": "4544@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "22",
+                "name": "李小明9",
+                "email": "2319@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "23",
+                "name": "李小明10",
+                "email": "7962@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "24",
+                "name": "花蓮雪莉",
+                "email": "2854@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "25",
+                "name": "高雄郭郭",
+                "email": "383@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "26",
+                "name": "李小明13",
+                "email": "3352@gmail.com",
+                "mobile": "0918111222",
+                "birthday": "1990-05-07",
+                "address": "台南市",
+                "created_at": "2020-04-06 16:31:02"
+            },
+            {
+                "sid": "27",
+                "name": "方大同",
+                "email": "5613@gmail.com",
+                "mobile": "0918-222555",
+                "birthday": "1990-04-01",
+                "address": "新北市",
+                "created_at": "2020-04-08 11:50:22"
+            },
+            {
+                "sid": "28",
+                "name": "哈哈哈",
+                "email": "8007@gmail.com",
+                "mobile": "0918-222333",
+                "birthday": "0000-00-00",
+                "address": "<script>alert(\"爛芭樂!!!\")<\/script>",
+                "created_at": "2020-04-08 12:05:21"
+            },
+            {
+                "sid": "29",
+                "name": "gdsgdsfg",
+                "email": "3196@gmail.com",
+                "mobile": "0918-222333",
+                "birthday": "2020-04-08",
+                "address": "dfgdfg\r\n                    ",
+                "created_at": "2020-04-09 14:56:05"
+            },
+            {
+                "sid": "30",
+                "name": "哈老大",
+                "email": "1959@gmail.com",
+                "mobile": "0918-222333",
+                "birthday": "2020-04-01",
+                "address": "哈老大",
+                "created_at": "2020-04-09 15:11:01"
+            },
+            {
+                "sid": "33",
+                "name": "方大同",
+                "email": "206@gmail.com",
+                "mobile": "0918-222555",
+                "birthday": "2020-04-01",
+                "address": "asdfasd",
+                "created_at": "2020-04-09 16:31:09"
+            },
+            {
+                "sid": "34",
+                "name": "林小白",
+                "email": "5155@gmail.com",
+                "mobile": "0999-123-456",
+                "birthday": "1995-05-05",
+                "address": "台北市",
+                "created_at": "2020-04-10 09:23:56"
+            },
+            {
+                "sid": "36",
+                "name": "dfhdfh",
+                "email": "5156@gmail.com",
+                "mobile": "fghf",
+                "birthday": "2020-04-09",
+                "address": "fgh",
+                "created_at": "2020-04-10 10:12:09"
+            },
+            {
+                "sid": "37",
+                "name": ">sdfsdasd",
+                "email": "316@gmail.com",
+                "mobile": "0918-222333",
+                "birthday": "2020-04-01",
+                "address": "sdfgsd",
+                "created_at": "2020-04-10 14:03:13"
+            }
+        ]
+    }
+]
